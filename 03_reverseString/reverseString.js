2b9e334946b9c2f398165594c4f21684752feb65
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-10-11 21:58:07 +0100

    reverse string task completed and passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = string.split('').reverse().join('');
+    return reverse;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
