27644245532ef91e19e5720e6a5889592ebde4f4
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-10-11 21:39:00 +0100

    javascript exercises helloWorld and repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+let string = "";
 
+const repeatString = function(words,num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+
+    let string = '';
+
+    for (let i = 0; i < num; i++) {
+        string += words;
+    }
+    return string;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
