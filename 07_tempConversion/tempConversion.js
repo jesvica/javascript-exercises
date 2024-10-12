a1ec0026828d910f22d1486bf2af8f30bf63127f
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-10-12 23:29:30 +0100

    completed task 7 - converting celsius to fahrenheit and fahrenheit to celsius, to one decimal number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5/9;
+    let rounded;
+
+    if (celsius % 1 === 0) {
+      rounded = Math.ceil(celsius);
+    } else {
+      rounded = Number(celsius.toFixed(1));
+    }
+    
+    return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  let rounded;
+
+  if (fahrenheit % 1 === 0) {
+    rounded = Math.ceil(fahrenheit)
+  } else {
+    rounded = Number(fahrenheit.toFixed(1))
+  }
+
+  return rounded;
 };
 
 // Do not edit below this line
