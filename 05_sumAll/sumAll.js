c832f1b66cd4d75ad6928e51f2a22159945b90c0
Jessica Williams <jvictoriaw@hotmail.co.uk>
2024-10-12 22:17:34 +0100

    completed taks 5 using if else statement and for loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let finalSum = 0;
 
+    if (start <= 0 || end <= 0 || typeof start !== 'number' || typeof end !== 'number') {
+        return "ERROR"
+    } else if (start < end) {
+        for (let counter = start; counter <= end; counter++) {
+            finalSum += counter;
+        }
+    } else if (end < start) {
+        for (let counter = end; counter <= start; counter++) {
+            finalSum += counter;
+        }
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
